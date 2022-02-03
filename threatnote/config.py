da05231c901b9c4c1b43e5f84185fc594abb8745
Matthew Gall <me@matthewgall.dev>
2022-02-03 19:22:35 +0000

    Adding option to import SERVER_NAME from an environment variable
    Source: https://github.com/brianwarehime/threatnote/pull/22

--- threatnote/config.py
+++ threatnote/config.py
@@ -13,7 +13,7 @@ def create_app():
 
     basedir = os.path.abspath(os.path.dirname(__file__))
     load_dotenv(os.path.join(basedir, '.env'))
-    app.config['SERVER_NAME'] = 'local.docker:5000'
+    app.config['SERVER_NAME'] = os.environ.get('SERVER_NAME') or 'local.docker:5000'
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'no_secret_key_set'
